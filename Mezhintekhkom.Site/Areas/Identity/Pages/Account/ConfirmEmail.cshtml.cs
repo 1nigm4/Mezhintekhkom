ab4203fc0efa9757207305f655329c34513942f8
1nigm4 <geloyzer@gmail.com>
2022-12-17 20:34:23 +0300

    Add state of result

--- Mezhintekhkom.Site/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ Mezhintekhkom.Site/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -1,22 +1,18 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
+﻿#nullable disable
 
-using System;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Mezhintekhkom.Site.Data.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
 
 namespace Mezhintekhkom.Site.Areas.Identity.Pages.Account
 {
     public class ConfirmEmailModel : PageModel
     {
+        [TempData]
+        public string StatusMessage { get; set; }
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
 
@@ -26,30 +22,28 @@ namespace Mezhintekhkom.Site.Areas.Identity.Pages.Account
             _signInManager = signInManager;
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [TempData]
-        public string StatusMessage { get; set; }
         public async Task<IActionResult> OnGetAsync(string id, string code, string returnUrl = null)
         {
+            returnUrl ??= Url.Page("./Manage/Index");
+
             if (id == null || code == null)
             {
                 return LocalRedirect(returnUrl);
             }
 
-            returnUrl = returnUrl ?? Url.Content("~/");
-
             var user = await _userManager.FindByIdAsync(id);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{id}'.");
+                await _signInManager.SignOutAsync();
+                return RedirectToPage();
             }
 
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
             var result = await _userManager.ConfirmEmailAsync(user, code);
-            return RedirectToPage("./Login", new { Caption = result.Succeeded ? "Адрес электронной почты подтвержден" : "Ошибка подтверждения почты", ReturnUrl = returnUrl });
+            return RedirectToPage("./Login", new {
+                Caption = result.Succeeded ? "Адрес электронной почты подтвержден" : "Ошибка подтверждения почты",
+                State = result.Succeeded ? "success" : "danger",
+                ReturnUrl = returnUrl });
         }
     }
 }

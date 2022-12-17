2ddbd8237fab75a25f835b42b539b41834f0cc21
1nigm4 <geloyzer@gmail.com>
2022-12-17 20:37:36 +0300

    Create Identity controller Courses actions nav to Identity actions

---
+++ Mezhintekhkom.Site/Controllers/IdentityController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mezhintekhkom.Site.Controllers
+{
+    public class IdentityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Courses(Guid id)
+        {
+            if (id != default)
+                return View("Details");
+            return View();
+        }
+    }
+}
